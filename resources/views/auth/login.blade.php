3f7d2921cc4eec2754377ced3179844acbffbfc9
webuxmotion <pereverziev.andrii@gmail.com>
2025-02-27 00:05:24 +0200

    Add SCSS. Top header

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+<x-shop-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
@@ -48,4 +48,4 @@
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+</x-shop-layout>
---
+++ resources/views/partials/top-header.blade.php
@@ -0,0 +1,19 @@
+<div class="top-header section">
+    <div class="container h-full">
+        <div class="top-header__cells">
+            <div class="top-header__cell"></div>
+            <div class="top-header__cell">
+                <nav class="top-header__nav">
+                    <ul>
+                        <li>
+                            <a href="/register">Registration</a>
+                        </li>
+                        <li>
+                            <a href="/login">Login</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+</div>
