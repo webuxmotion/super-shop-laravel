45457f05b611117ea54c4157b4821aa9ca13a6cf
webuxmotion <pereverziev.andrii@gmail.com>
2025-03-03 02:47:37 +0200

    Auth forms

---
+++ resources/views/auth/partials/login-with-google.blade.php
@@ -0,0 +1,6 @@
+<div class="max-w-[350px] mx-auto w-full pt-10 flex justify-center flex-col items-center">
+    
+    <x-google-button />
+
+    <span class="mt-5">OR</span>
+</div>
---
+++ resources/views/components/auth-tabs.blade.php
@@ -0,0 +1,14 @@
+<div class="auth-tabs">
+    <ul>
+        <li>
+            <a href="/login" class="{{ $active === 'login' ? 'is-active' : '' }}">
+                Login
+            </a>
+        </li>
+        <li>
+            <a href="/register" class="{{ $active === 'register' ? 'is-active' : '' }}">
+                Registration
+            </a>
+        </li>
+    </ul>
+</div>
